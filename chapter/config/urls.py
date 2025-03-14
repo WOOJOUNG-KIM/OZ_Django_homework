d7dd66290dcf29e0058d4272a3702249fe5e6961
WOOJOUNG-KIM <dnwnd660@daum.net>
2025-03-14 09:58:15 +0900

    ✨feat: chapter_02 과제 업로드

--- chapter/config/urls.py
+++ chapter/config/urls.py
@@ -20,6 +20,7 @@ from django.shortcuts import render
 from django.urls import path
 from fake_db import user_db
 
+from todo.views import todo_list, todo_info
 
 _db = user_db
 
@@ -37,7 +38,9 @@ def user_info(request, user_id):
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('users/', user_list, name='user_list'),
     path('users/<int:user_id>/', user_info, name='user_info'),
-    path('admin/', admin.site.urls),
+    path('todo/', todo_list, name='todo_list'),  # 추가
+    path('todo/<int:todo_id>/', todo_info, name='todo_info'),  # 추가
 ]
