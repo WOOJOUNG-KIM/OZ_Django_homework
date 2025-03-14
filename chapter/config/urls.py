772e51f4a9b2666cf457bb23c985fb9950dc256f
WOOJOUNG-KIM <dnwnd660@daum.net>
2025-03-14 10:18:41 +0900

    ✨feat: chapter_03 과제 업로드

--- chapter/config/urls.py
+++ chapter/config/urls.py
@@ -14,33 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# config/urls.py
+
 from django.contrib import admin
-from django.http import Http404
-from django.shortcuts import render
-from django.urls import path
-from fake_db import user_db
-
+from django.urls import path, include
 from todo.views import todo_list, todo_info
-
-_db = user_db
-
-
-def user_list(request):
-    names = [{'id': key, 'name': value['이름']} for key, value in _db.items()]
-    return render(request, 'user_list.html', {'data': names})
-
-
-def user_info(request, user_id):
-    if user_id > len(_db):
-        raise Http404('User not found')
-    info = _db[user_id]
-    return render(request, 'user_info.html', {'data': info})
-
+from users import views as users_views
 
 urlpatterns = [
+    path('todo/', todo_list, name='todo_list'),
+    path('todo/<int:todo_id>/', todo_info, name='todo_info'),
     path('admin/', admin.site.urls),
-    path('users/', user_list, name='user_list'),
-    path('users/<int:user_id>/', user_info, name='user_info'),
-    path('todo/', todo_list, name='todo_list'),  # 추가
-    path('todo/<int:todo_id>/', todo_info, name='todo_info'),  # 추가
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', users_views.login, name='login'),
+    path('accounts/signup/', users_views.sign_up, name='signup')
 ]
+
+
